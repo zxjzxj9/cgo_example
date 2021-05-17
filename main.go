a131fc4348e0c5134041ba9790b4878b53c03f9a
Victor Zhang <zxjzxj9@gmail.com>
2021-05-17 23:10:56 +0800

    Break include file into multiples

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
-// #cgo CXXFLAGS: -I/usr/local/Cellar/libtorch/1.8.1/include -I/usr/local/Cellar/libtorch/1.8.1/include/torch/csrc/api/include -I/Users/stardust/miniconda3/envs/nnpot//include/python3.7m -std=c++14
+// #cgo CXXFLAGS: -I/usr/local/Cellar/libtorch/1.8.1/include
+// #cgo CXXFLAGS: -I/usr/local/Cellar/libtorch/1.8.1/include/torch/csrc/api/include
+// #cgo CXXFLAGS: -I/Users/stardust/miniconda3/envs/nnpot/include/python3.7m -std=c++14
 // #cgo LDFLAGS: -L/usr/local/Cellar/libtorch/1.8.1/lib -ltorch -lc10 -ltorch_cpu -ltorch_global_deps
 // #include "test.hh"
 import "C"
-
 import "fmt"
 
 func main() {
