4d409bf548ff2723d5667000d56f1d9fb91cd08a
Victor Zhang <zxjzxj9@gmail.com>
2021-05-19 22:16:30 +0800

    Modify the code to save a traced model

--- model/model.py
+++ model/model.py
@@ -6,10 +6,16 @@ import torch
 model = torch.hub.load('facebookresearch/pytorch_GAN_zoo:hub', 'DCGAN', pretrained=True, useGPU=False)
 num_images = 64
 noise, _ = model.buildNoiseData(num_images)
-with torch.no_grad():
-    generated_images = model.test(noise)
-
+model.netG.eval()
+print(type(model.netG))
+# print(noise.shape)
+# with torch.no_grad():
+# generated_images = model.test(noise)
 # let's plot these images using torchvision and matplotlib
-import matplotlib.pyplot as plt
-import torchvision
-plt.imshow(torchvision.utils.make_grid(generated_images).permute(1, 2, 0).cpu().numpy())
+m: torch.jit.TracedModule = torch.jit.trace(model.netG, (noise,))
+print(type(m))
+torch.jit.save(m, "dcgan.pth")
+# print(type(m))
+# import matplotlib.pyplot as plt
+# import torchvision
+# plt.imshow(torchvision.utils.make_grid(generated_images).permute(1, 2, 0).cpu().numpy())
