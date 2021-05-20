6b69ad07013b7ae31f1976121fee4b311b1a8348
Victor Zhang <zxjzxj9@gmail.com>
2021-05-20 22:43:37 +0800

    Add image generator

--- test.cc
+++ test.cc
@@ -1,6 +1,10 @@
 #include "test.hh"
+#include <torch/torch.h>
+#include <torch/script.h>
 #include <torch/extension.h>
 #include <iostream>
+
+auto m = torch::jit::load("./dcgan.pth");
 
 void print() {
     std::cout<<"Test2"<<std::endl;
@@ -16,4 +20,11 @@ void fillRandn(double* a, int sz) {
     for(int i=0; i<sz; i++) {
         a[i] = static_cast<float>(ptr[i]);
     }
+}
+
+// output bs x 64 x 64 x 3
+void genImage(unsigned char* output, int sz) {
+    auto a = torch::randn({16, 120});
+    auto ret = m.forward({a});
+    auto b = (ret.toTensor() * 256).to(torch::kUInt8);
 }
